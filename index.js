da3d57fb084ae0eafc3758732591856b6767fa05
joseph reis <josephreis646@gmail.com>
2023-03-02 16:44:27 -0500

    Created a function to write a README

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -52,7 +53,11 @@ const questions = [
   
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+      err ? console.error(err) : console.log('README file generated successfully!')
+    );
+  }
 
 // TODO: Create a function to initialize app
 function init() {}
